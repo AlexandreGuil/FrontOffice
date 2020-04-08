5c1feaa2e9e834162c0df854e43fb95c0873aad1
Flora Dupont <flora.dupont@hotmail.fr>
2020-04-08 18:56:47 +0200

    ajout component modif catégorie

--- FrontOffice/src/app/app.module.ts
+++ FrontOffice/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarreComponent } from './nav-barre/nav-barre.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AuthentificationComponent } from './authentification/authentification.component';
+import { ModifCategorieComponent } from './modif-categorie/modif-categorie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarreComponent,
     NavigationComponent,
-    AuthentificationComponent
+    AuthentificationComponent,
+    ModifCategorieComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontOffice/src/app/modif-categorie/modif-categorie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modif-categorie',
+  templateUrl: './modif-categorie.component.html',
+  styleUrls: ['./modif-categorie.component.css']
+})
+export class ModifCategorieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
