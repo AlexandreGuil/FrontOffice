bcd93b95133ee66c3d44c6b9d4157e1384fae6cf
Lucas Fontaine <lucasfontaine@orange.fr>
2020-04-08 17:35:18 +0200

    Ajout javascript

---
+++ src/js/panier.js
@@ -0,0 +1,17 @@
+function supprimer(){
+    let choix = confirm("Êtes-vous sûr de vouloir supprimer?");
+    if(choix){
+        // Mettez ici la logique de suppression
+    }
+}
+
+function enregistrer(){
+  let enregistr = confirm("Voulez-vous enregistrer votre panier?");
+  if(enregistr){
+      // Mettez ici la logique d'enregistrement
+  }
+}
+
+function payer(){
+  alert("Merci de votre visite");
+}
