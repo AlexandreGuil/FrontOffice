885b8f9f14446d7cbe20422c64c010a561d5da9d
Flora Dupont <flora.dupont@hotmail.fr>
2020-04-09 19:03:14 +0200

    manip de merde sur el panier

--- FrontOffice/src/app/panier-final/panier-final.component.ts
+++ FrontOffice/src/app/panier-final/panier-final.component.ts
@@ -12,4 +12,25 @@ export class PanierFinalComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+ supprimer(){
+    let choix = confirm("Êtes-vous sûr de vouloir supprimer?");
+    if(choix){
+        // Mettez ici la logique de suppression
+    }
+}
+
+enregistrer(){
+  let enregistr = confirm("Voulez-vous enregistrer votre panier?");
+  if(enregistr){
+      // Mettez ici la logique d'enregistrement
+  }
+}
+
+payer(){
+  alert("Merci de votre visite");
+}
+
+
+
 }
