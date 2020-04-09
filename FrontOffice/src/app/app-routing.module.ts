c304c04b7a630e08ef1cb4278cc77a4a19452168
Flora Dupont <flora.dupont@hotmail.fr>
2020-04-09 17:06:29 +0200

    transfert authentification html vers angular + création des routes

--- FrontOffice/src/app/app-routing.module.ts
+++ FrontOffice/src/app/app-routing.module.ts
@@ -2,7 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { NavBarreComponent } from './nav-barre/nav-barre.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { ModifCategorieComponent } from './modif-categorie/modif-categorie.component';
+import { PanierFinalComponent } from './panier-final/panier-final.component';
+
+
+const routes: Routes = [
+{path : 'authentification', component: AuthentificationComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
