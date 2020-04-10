43db308510a92f9ca29368d08d55baf2bb513d07
Flora Dupont <flora.dupont@hotmail.fr>
2020-04-10 22:13:40 +0200

    ajout d'un service vide client

---
+++ FrontOffice/src/service/client.service.ts
@@ -0,0 +1,13 @@
+import{Injectable} from '@angular/core';
+
+import { map } from 'rxjs/operators';
+import { observable } from 'rxjs';
+
+@Injectable ({providedIn : 'root'})
+export class ClientService{
+
+clients: any []=[];
+
+constructor (){}
+
+}
