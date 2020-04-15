da92e14d5b5c8a3791f06828a9489e2e0144de93
Lucas Fontaine <lucasfontaine@orange.fr>
2020-04-16 00:34:58 +0200

    Resolution conflit Flora

--- FrontOffice/src/app/app.module.ts
+++ FrontOffice/src/app/app.module.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-=======
-import { HttpClientModule } from '@angular/common/http';
-import { ClientService } from 'src/service/client.service';
->>>>>>> ca0d0ffc813109158959657e5e336f160011ff91
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,13 +9,9 @@ import { ModifCategorieComponent } from './modif-categorie/modif-categorie.compo
 import { PanierFinalComponent } from './panier-final/panier-final.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InscriptionComponent } from './inscription/inscription.component';
-<<<<<<< HEAD
 import { InscriptionService } from 'src/services/inscription.service';
 import { HttpClientModule } from '@angular/common/http';
-=======
 import { HomeComponent } from './home/home.component';
-
->>>>>>> ca0d0ffc813109158959657e5e336f160011ff91
 
 
 
@@ -40,19 +30,11 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-<<<<<<< HEAD
     ReactiveFormsModule,
     HttpClientModule
     
   ],
   providers: [InscriptionService],
-=======
-    HttpClientModule,
-    ReactiveFormsModule
-    
-  ],
-  providers: [ClientService],
->>>>>>> ca0d0ffc813109158959657e5e336f160011ff91
   bootstrap: [AppComponent]
 })
 export class AppModule { }
